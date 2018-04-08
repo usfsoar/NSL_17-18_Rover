134e179bbd35a0043b0a0af9dc2b241cc3eb504a
Lott-Kerby <lalime@mail.usf.edu>
2018-04-07 21:47:03 -0400

    Forward rover movement
    Code to test rover movement

---
+++ motor_test.ino
@@ -0,0 +1,60 @@
+void rove(){
+
+  digitalWrite(23, HIGH);
+  digitalWrite(22, LOW);
+  analogWrite(8, 255);
+  
+  digitalWrite(24, LOW);
+  digitalWrite(25, HIGH);
+  analogWrite(9, 255);
+
+  delay(4000);
+   digitalWrite(23, LOW);
+  digitalWrite(22, LOW);
+
+  
+  digitalWrite(24, LOW);
+  digitalWrite(25, LOW);
+  return;
+  }
+
+
+
+void setup() {
+
+  pinMode(8, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+
+  pinMode(9, OUTPUT);
+  pinMode(24, OUTPUT);
+  pinMode(25, OUTPUT);
+}
+
+void loop() {
+
+  while (Serial.available()) {                     //as long as there is data on incoming serial buffer...
+
+
+    //Characers are read from the serial buffer and concatinated 
+    char c = Serial.read();
+    readString += c;
+    delay(2);
+  }
+  
+  if(readString ==" rover deployed."){
+    
+    
+      Serial.print("Rover is roving..."); 
+
+      rove();
+
+      Serial.print( "Hope its 5 feet");
+      return;
+-*      
+      }
+
+      else Serial.print(readString);
+      readString="";
+    
+}
